3042fe2ca7d5b7fc5f6334db04c4d7976be3d6f0
Andrei Berceanu <andreicberceanu@gmail.com>
2023-08-21 19:16:55 +0300

    Add dependencies

--- Tools/machines/karolina-it4i/install_dependencies.sh
+++ Tools/machines/karolina-it4i/install_dependencies.sh
@@ -41,3 +41,5 @@ python3 -m pip install --upgrade openpmd-api
 python3 -m pip install --upgrade openpmd-viewer
 python3 -m pip install --upgrade mpi4py --no-cache-dir --no-build-isolation --no-binary mpi4py
 python3 -m pip install --upgrade numba
+python3 -m pip install --upgrade mako
+python3 -m pip install --upgrade pyopencl
